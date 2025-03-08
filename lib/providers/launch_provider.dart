63929424d1603daaa2aa0c55cea2a26be3f2d6e8
SVG21 <bhandarisarvagya21@gmail.com>
2025-03-08 16:57:45 +0000

    added launch provider

---
+++ lib/providers/launch_provider.dart
@@ -0,0 +1,23 @@
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:spacex_flutter_app/models/launch.dart';
+import 'package:spacex_flutter_app/services/spacex_service.dart';
+
+final spaceXServiceProvider = Provider((ref) => SpaceXService());
+
+final launchesProvider = FutureProvider<List<Launch>>((ref) async {
+  final service = ref.read(spaceXServiceProvider);
+  return await service.fetchLaunches();
+});
+
+final selectedYearProvider = StateProvider<int?>((ref) => null);
+
+final filteredLaunchesProvider = FutureProvider<List<Launch>>((ref) async {
+  final service = ref.read(spaceXServiceProvider);
+  final launches = await service.fetchLaunches();
+  final selectedYear = ref.watch(selectedYearProvider);
+  return selectedYear != null
+      ? launches
+          .where((launch) => launch.launchDate.year == selectedYear)
+          .toList()
+      : launches;
+});
